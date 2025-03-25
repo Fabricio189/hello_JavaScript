a308626c4483eb95fec6407918fde0c506330266
Fabri Joss <cerdafabricio1989@gmail.com>
2025-03-24 22:13:03 -0500

    Actualizacion de variables y tipos de datos.

--- BASIC/01-variables.js
+++ BASIC/01-variables.js
@@ -15,3 +15,10 @@ helloWorld2 = "¡Hola de nuevo JavaScript 2"
 console.log(helloWorld2)
 
 // const
+
+const helloWorld3 = "¡Hola, JavaScript 3"
+console.log(helloWorld3)
+
+//ERROR
+//helloWorld3 = "¡Hola de nuevo JavaScript 3" // NO SE PUEDE REASIGNAR
+//console.log(helloWorld3)
---
+++ BASIC/02-tiposDatos.js
@@ -0,0 +1,57 @@
+// TIPOS DE DATOS PRIMITIVOS
+
+//Cadena de texto (String)
+
+let myName = "Fabricio José"
+let alias = 'CodeJoss'
+let email = 'codejoss@gmail.com'
+
+// Números (Number)
+
+let age = 35 // entero
+let height = 1.68 // Decimal
+
+// Booleanos (bnoolean)
+
+let isTeacher = true
+let isStudent = false
+
+// Undefined
+
+let UndefinedValue
+console.log(UndefinedValue)
+
+// Null
+
+let nullValue = null
+
+// Symbol
+
+let mySymbol = Symbol("mysymbol")
+
+// BigInt
+
+let myBigInt = BigInt(4378279367380368478904780780)
+let myBigInt2 = 4378279367380368478904780780n
+
+// Mostramos los tipos de datos
+console.log(typeof myName)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof height)
+
+console.log(typeof isTeacher)
+console.log(typeof isStudent)
+
+console.log(typeof UndefinedValue)
+
+console.log(typeof nullValue)
+
+console.log(typeof mySymbol)
+
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
+
+
